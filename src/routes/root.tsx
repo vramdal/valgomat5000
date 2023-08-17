9f2cee9a4d59211e81b1b25c5160cd9edcaa8c25
Vidar Ramdal <vramdal@gmail.com>
2023-08-17 11:08:05 +0200

    Versjon 1.7 - dedikasjon

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,7 +5,7 @@ const Root = () => {
   const form = useContext(FormContext);
   return (
     <>
-      <h1>Valgomat5000 - 2023-utgaven</h1>
+      <h1>Roberts valgomat - 2023-utgaven</h1>
       <img src={"/valgomat5000/standard_stemmerett.jpg"} alt={"Dame som putter stemmeseddel i valgboks. Bilde: Anders Beer Wilse"}/>
       <Step>
         <p>Først noen bakgrunnsspørsmål.</p>
