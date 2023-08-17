4c5ea3c539e881ffe037f7d6616cb1d82452defe
Vidar Ramdal <vramdal@gmail.com>
2023-08-17 11:24:26 +0200

    Versjon 1.8 - bildestyling

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -6,13 +6,17 @@ const Root = () => {
   return (
     <>
       <h1>Roberts valgomat - 2023-utgaven</h1>
-      <img src={"/valgomat5000/standard_stemmerett.jpg"} alt={"Dame som putter stemmeseddel i valgboks. Bilde: Anders Beer Wilse"}/>
       <Step>
+        <img src="/valgomat5000/standard_stemmerett.jpg" style={{maxHeight: "40vh", display: "block", alignSelf: "center"}} alt={"Dame som putter stemmeseddel i urnen. Foto: Anders B. Wilse"}/>
         <p>Først noen bakgrunnsspørsmål.</p>
         <h2>Er du klar?</h2>
         <fieldset className={"choices"}>
-          <label><input type={"radio"} required={true} {...(form.register('klar', {validate: (value) => value === "Ja"}))} value={'Ja'}/>Ja</label>
-          <label><input type={"radio"} required={true} {...(form.register('klar', {validate: (value) => value === "Ja"}))} value={'Nei'}/>Nei</label>
+          <label><input type={"radio"}
+                        required={true} {...(form.register('klar', {validate: (value) => value === "Ja"}))}
+                        value={'Ja'}/>Ja</label>
+          <label><input type={"radio"}
+                        required={true} {...(form.register('klar', {validate: (value) => value === "Ja"}))}
+                        value={'Nei'}/>Nei</label>
         </fieldset>
         <aside>Ingen data sendes noe sted</aside>
       </Step>
